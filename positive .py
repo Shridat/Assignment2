a2d5baa07ab5eca21755e5ee6fd33190f195bb6f
Shridat <80768804+Shridat@users.noreply.github.com>
2021-06-06 22:12:13 +0530

    Create positive .py

---
+++ positive .py
@@ -0,0 +1,12 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+print("list1=",list1)
+i=0
+for i in range(0,len(list1)):
+    if(list1[i]>0):
+      print(list1[i])
+
+print("list2=",list2)
+for i in range(0,len(list2)):
+    if(list2[i]>0):
+      print(list2[i])
